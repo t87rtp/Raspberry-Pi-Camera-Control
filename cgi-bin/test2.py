d1c147621d2cc1829a8cd389814cf6e331ab52e3
yana <t87rtp@gmail.com>
2017-09-11 02:39:39 +0900

    query for size

--- cgi-bin/test2.py
+++ cgi-bin/test2.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
 
 import os
+import urllib
+import urlparse
+import io
+import picamera
 
-print "Content-type: text/html";
+q_str = os.environ.get("QUERY_STRING", "")
+q = urlparse.parse_qs(q_str)
+
+my_stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    camera.resolution = (q["w"],q["h"])
+    #camera.start_preview()
+    # Camera warm-up time
+    #time.sleep(2)
+#    camera.capture('/home/pi/nas/DCIM/foo.jpg')
+    camera.capture(my_stream, 'jpeg')
+
+#print "Content-type: text/html";
+#print
+#print "<font size=+1>Environment</font><\br>";
+#for param in os.environ.keys():
+#    print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
+print "Content-type: image/jpeg"
 print
-print "<font size=+1>Environment</font><\br>";
-for param in os.environ.keys():
-    print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
+print my_stream.getvalue()
