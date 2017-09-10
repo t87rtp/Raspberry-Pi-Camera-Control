77fe857b87205f1e14f99a4f490daaea51dcbe5b
yana <t87rtp@gmail.com>
2017-09-11 02:10:25 +0900

    test

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -7,9 +7,9 @@ import picamera
 my_stream = io.BytesIO()
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
-    camera.start_preview()
+    #camera.start_preview()
     # Camera warm-up time
-    time.sleep(2)
+    #time.sleep(2)
 #    camera.capture('/home/pi/nas/DCIM/foo.jpg')
     camera.capture(my_stream, 'jpeg')
 
