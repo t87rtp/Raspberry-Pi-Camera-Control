d4b981f5171c0636c4be23d63d4892cefb98bf3e
yana <t87rtp@gmail.com>
2017-09-11 02:28:57 +0900

    test2

--- cgi-bin/test2.py
+++ cgi-bin/test2.py
@@ -2,7 +2,8 @@
 
 import os
 
-print "Content-type: text/html\r\n\r\n";
+print "Content-type: text/html";
+print
 print "<font size=+1>Environment</font><\br>";
 for param in os.environ.keys():
     print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
