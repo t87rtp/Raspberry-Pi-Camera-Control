fd55bfe9b80104aef1b96bd1a362387bca908026
yana <t87rtp@gmail.com>
2017-09-11 18:58:58 +0900

    power.py test3

--- cgi-bin/power.py
+++ cgi-bin/power.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 
 import os
+import urllib
+import urlparse
+
+q_str = os.environ.get("QUERY_STRING", "")
+q = urlparse.parse_qs(q_str)
+command = "sudo shutdown -r now"
+if q["mode"]:
+	if q["mode"][0] == "reboot":
+		command = "sudo reboot"
+
 print "Content-type: text/html";
 print
 print "reboot"
 #print "<font size=+1>Environment</font><\br>";
 #for param in os.environ.keys():
 #    print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
-os.system("sudo shutdown -r now")
+os.system(command)
