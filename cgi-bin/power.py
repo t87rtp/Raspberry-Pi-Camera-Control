b23dc99109233df3feacfc1c4aa9ae7d596ad8ef
yana <t87rtp@gmail.com>
2017-09-11 18:52:05 +0900

    power test

--- cgi-bin/power.py
+++ cgi-bin/power.py
@@ -3,6 +3,8 @@
 import os
 print "Content-type: text/html";
 print
-print "<font size=+1>Environment</font><\br>";
-for param in os.environ.keys():
-    print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
+print "reboot"
+#print "<font size=+1>Environment</font><\br>";
+#for param in os.environ.keys():
+#    print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
+os.system("sudo shutdown -r now")
